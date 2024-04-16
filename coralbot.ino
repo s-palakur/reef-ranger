1e2831e3bd30b48caa719de64bec88c1c0a315c5
s-palakur <117892017+s-palakur@users.noreply.github.com>
2024-04-16 16:18:13 -0700

    Code with vacumn pump and valve control.

--- coralbot.ino
+++ coralbot.ino
@@ -1,13 +1,21 @@
-const int pumpPin =  9;  
- 
+const int valve_pin = 8; 
+const int pumpPin_in =  10;  
+const int pumpPin_out = 9; 
+
  
 void setup() {
-  pinMode(pumpPin, OUTPUT);
+  pinMode(pumpPin_in, OUTPUT);
+  pinMode(pumpPin_out, OUTPUT);
+  pinMode(valve_pin, OUTPUT);
 }
  
 void loop() {
-    digitalWrite(pumpPin, HIGH);  
-    delay(500);                   
-    digitalWrite(pumpPin, LOW);    
-    delay(5000);              
+    digitalWrite(valve_pin, LOW);
+    analogWrite(pumpPin_in, 75);
+    analogWrite(pumpPin_out, 0);  
+    delay(500);    
+    digitalWrite(valve_pin, HIGH);
+    analogWrite(pumpPin_in, 0);  
+    analogWrite(pumpPin_out, 255);  
+    delay(3000);              
 }
